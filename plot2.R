afba5df6d2cb08171f06f44ce51ba63eda16b0a7
dkfang <dkfalex@gmail.com>
2017-09-11 23:30:40 -0700

    updated codes for plots

--- plot2.R
+++ plot2.R
@@ -1,3 +1,38 @@
+# Assignment1
+# DongKai Fang
+# Exploratory Data Analysis
+
+mydata <- read.table(file = "household_power_consumption.txt",
+                     sep = ";", header = TRUE, na.strings = "?",
+                     colClasses = c("character","character", rep("numeric",7)))
+
+# check
+head(mydata)
+
+# Now, we convert first variable from character to Date
+mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y")
+
+# check
+str(mydata$Date)
+
+# Subsetting data from 2007-02-01 and 2007-02-02
+mydata <- subset(mydata, Date >= "2007-2-1" & Date <= "2007-2-2")
+
+# Convert the Date and Time variables to Date/Time classes 
+dataTime <- paste(mydata$Date, mydata$Time)
+
+# remove columns (date and time) and combine a new "Date/Time" column
+mydata <- mydata[ ,!(names(mydata) %in% c("Date","Time"))]
+mydata <- cbind(dataTime, mydata)
+
+#removing NA values
+mydata <- mydata[complete.cases(mydata),]
+
+## Format dateTime Column
+mydata$dataTime <- as.POSIXct(dataTime)
+# check
+head(mydata)
+
 ## Plot 2
 plot(mydata$Global_active_power~mydata$dataTime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
